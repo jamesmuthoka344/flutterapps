6c26e6efe900cd1939cc27694fa788177bb83557
web designer <115144831+jamesmuthoka344@users.noreply.github.com>
2026-05-08 05:56:28 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'screens/home_screen.dart';
+import 'services/database_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    debugPrint('Initializing FFI...');
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+    debugPrint('FFI initialized');
+  }
+  await DatabaseService().initDatabase();
+  runApp(
+    ProviderScope(
+      child: BibleApp(),
+    ),
+  );
+}
+
+class BibleApp extends StatelessWidget {
+  const BibleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bible App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
